26e27895e4a2c32bf966dbc9de97885e40f0697e
Geoff M <geoffroym2@gmail.com>
2018-05-09 11:54:54 +0200

    1.0

--- src/Fenetre.java
+++ src/Fenetre.java
@@ -1,9 +1,19 @@
+import javax.swing.JFrame;
+
 
 
 public class Fenetre {
 
 	public Fenetre() {
 		// TODO Auto-generated constructor stub
+	}
+	public static void main(String[] args){
+		JFrame f=new JFrame();
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setSize(700, 500);
+		
+		f.pack();
+		f.setVisible(true);
 	}
 
 }
--- src/PanneauChoix.java
+++ src/PanneauChoix.java
@@ -1,10 +1,15 @@
 
+import javax.swing.ButtonGroup;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 
 public class PanneauChoix extends JPanel{
-
+	private JRadioButton bnew = new JRadioButton("Nouvelle figure");
+	private JRadioButton bmain = new JRadioButton("Tracés à main levée");
+	private JRadioButton bmanip = new JRadioButton("Manipulations");
 	public PanneauChoix() {
-		// TODO Auto-generated constructor stub
+		add(bnew);
+		add(bmain);
+		add(bmanip);
 	}
-
 }

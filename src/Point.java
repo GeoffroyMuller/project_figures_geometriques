35a3d385bc331572211d36521cb7928d619ac859
Theo Biancalana <theobiancalana@gmail.com>
2018-05-14 10:52:36 +0200

    Classe point

---
+++ src/Point.java
@@ -0,0 +1,53 @@
+import java.util.*;
+public class Point {
+private int x,y;
+
+public Point(){
+	x=0;
+	y=0;
+}
+
+
+public Point(int px , int py){
+	x=px;
+	y=py;
+}
+
+public double distance(Point p){
+	int px = p.rendreX();
+	int py = p.rendreY();
+	Double res = Math.sqrt((px-x)*(px-x)+(py-y)*(py-y));
+	
+	return res;
+}
+
+public int rendreX(){
+	return x;
+}
+
+public int rendreY(){
+	return y;
+}
+
+public void incrementerX(){
+	x=x+1;
+}
+
+public void incrementerY(){
+	y=y+1;
+}
+
+public void modifierX(int mod){
+	x=mod;
+}
+
+public void modifierY(int mod){
+	y=mod;
+}
+
+public void translation(int tx,int ty){
+	x=x+tx;
+	y=y+ty;
+}
+
+}

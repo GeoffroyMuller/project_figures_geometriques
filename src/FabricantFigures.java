ea8d72695ebc486678530aa077e33fefb48f50c8
Geoff M <geoffroym2@gmail.com>
2018-05-28 11:48:15 +0200

    modification fabricantfigure

--- src/FabricantFigures.java
+++ src/FabricantFigures.java
@@ -23,25 +23,6 @@ public class FabricantFigures {
 			@Override
 			public void mousePressed(MouseEvent e) {
 				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			public void mouseExited(MouseEvent e) {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				// TODO Auto-generated method stub
-				System.out.println("fc.nbClics():"+fc.nbClics());
 				nbClics++;
 				System.out.println("clicked");
 				point = new Point(e.getX(),e.getY());
@@ -59,6 +40,40 @@ public class FabricantFigures {
 
 					}
 				}
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// TODO Auto-generated method stub
+				/*nbClics++;
+				System.out.println("clicked");
+				point = new Point(e.getX(),e.getY());
+
+				if(nbClics < fc.nbClics()){
+					lp.add(point);
+					System.out.println("addp"+nbClics);
+				}else{
+					if(nbClics == fc.nbClics()){
+						lp.add(point);
+						fc.modifierPoints(lp);//a changer
+						System.out.println("addpFinal"+nbClics);
+						System.out.println("insertion dans fc");
+						dm.ajoute(fc);
+
+					}
+				}*/
 				
 			}
 		};

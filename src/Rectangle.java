06ce4f3c2c082ff9f38012dfcd3b9669f44dd21e
Theo Biancalana <theobiancalana@gmail.com>
2018-05-28 11:12:04 +0200

    maj nbclic

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,4 +1,25 @@
+import java.awt.Graphics;
+import java.util.ArrayList;
 
-public class Rectangle {
 
+public class Rectangle extends Quadrilatere
+{
+	public Rectangle(){
+		super();
+	}
+
+	@Override
+	public int nbClics() {
+		return 2;
+	}
+	
+	public void modifierPoints(ArrayList<Point> tab_mem){
+		
+	}
+	
+
+	
+	public void transformation(int a,int b,int c){
+		
+	}
 }

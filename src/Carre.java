d2b98d4238371d43d489899e745e434954988cdb
theo_B <theobiancalana@gmail.com>
2018-06-09 12:55:10 +0200

    deformation operationnel pour carre et Losange

--- src/Carre.java
+++ src/Carre.java
@@ -1,15 +1,20 @@
 import java.util.ArrayList;
 
 public class Carre extends Quadrilatere{
-
+	boolean manipInverse;
 	public Carre() {
 		super();
 	}
 
 	public void modifierPoints(ArrayList<Point> ptab_mem){
+		System.out.println("modif normal");
 		this.tab_mem= ptab_mem;
 		Point p1 = tab_mem.get(0);
 		Point p2 = tab_mem.get(1);
+		if(tab_mem.size()>=2 && tab_mem.size()<4) {
+			tab_mem.add(null);
+			tab_mem.add(null);
+		}
 
 		//vecteur u utiliser pour la creation du carre
 		Point up1p2 = new Point();
@@ -28,22 +33,12 @@ public class Carre extends Quadrilatere{
 		v.modifierY(0-up1p2.rendreX());
 
 		Point p4 = new Point();
-		p4.translation(p1.rendreX()+v.rendreX(), p1.rendreY()+v.rendreY());
+		p4.translation(p1.rendreX()-v.rendreX(), p1.rendreY()-v.rendreY());
 
 		Point p3 = new Point();
-		p3.translation(p2.rendreX()+v.rendreX(),p2.rendreY()+v.rendreY());
-		tab_mem.add(p3);
-		tab_mem.add(p4);
-		System.out.println("==============");
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p1.distance(p2));
-		System.out.println(p3);
-		System.out.println(p4);
-		System.out.println("===================");
-		System.out.println(up1p2);
-		System.out.println(v);
-		System.out.println("/////////////");
+		p3.translation(p2.rendreX()-v.rendreX(),p2.rendreY()-v.rendreY());
+		tab_mem.set(2,p3);
+		tab_mem.set(3,p4);
 
 		System.out.println(tab_mem);
 
@@ -58,58 +53,61 @@ public class Carre extends Quadrilatere{
 		p3=tab_mem.get(3);
 		double dist;
 		ArrayList<Point> ptab_mem = new ArrayList<Point>();
-		if(p.distance(p2)<TAILLE_CARRE_SELECTION) {
-			Point up0p2 = new Point();//vecteur
-			up0p2.modifierX(p2.rendreX()-p0.rendreX());
-			up0p2.modifierY(p2.rendreY()-p0.rendreY());
-
-			Point up3p1 = new Point();//vecteur
-			up3p1.modifierX(p1.rendreX()-p3.rendreX());
-			up3p1.modifierY(p1.rendreY()-p3.rendreY());
-
-			while(Math.abs(up3p1.rendreX())!=Math.abs(up0p2.rendreX())) {
-				if(up3p1.rendreX()>=0){
-
-
-					p1.translation(1, 0);
-				}
-				else {
-					p1.translation(-1, 0);
-				}
-				System.out.println("iteration sur x");
-				up3p1.modifierX(p1.rendreX()-p3.rendreX());
-			}
-			
-			while(Math.abs(up3p1.rendreY())!=Math.abs(up0p2.rendreY())) {
-				if(up3p1.rendreY()>=0){
-
-
-					p1.translation(0, 1);
-				}
-				else {
-					p1.translation(0, -1);
-				}
-				System.out.println("iteration sur y");
-				up3p1.modifierX(p1.rendreY()-p3.rendreY());
-			}
-
-
+		ptab_mem.add(null);
+		ptab_mem.add(null);
+		ptab_mem.add(null);
+		ptab_mem.add(null);
+		
+		if((p!=p0 && p!=p1)) {
+			ptab_mem.set(3,p3);
+			ptab_mem.set(2,p2);
+			this.modifierPointsInverse(ptab_mem);
 		}
 		else {
-			if(p.distance(p3)<TAILLE_CARRE_SELECTION) {
-
-			}
-			else {
-			}
-		}
-		ptab_mem.add(p0);
-		ptab_mem.add(p1);
+		ptab_mem.set(0,p0);
+		ptab_mem.set(1,p1);
 		this.modifierPoints(ptab_mem);
+		}
+		
 	}
 
 
 	public int nbClics() {
 		return 2;
+	}
+	
+	public void modifierPointsInverse(ArrayList<Point> ptab_mem){
+		System.out.println("modif inverse");
+		this.tab_mem= ptab_mem;
+		Point p1 = tab_mem.get(2);
+		Point p2 = tab_mem.get(3);
+
+		//vecteur u utiliser pour la creation du carre
+		Point up1p2 = new Point();
+		up1p2.modifierX(p2.rendreX()-p1.rendreX());
+		up1p2.modifierY(p2.rendreY()-p1.rendreY());
+
+		double d = p1.distance(p2);
+
+		//vecteur v pour translation
+		Point v = new Point();
+
+		
+
+
+		v.modifierX(up1p2.rendreY());
+		v.modifierY(0-up1p2.rendreX());
+
+		Point p4 = new Point();
+		p4.translation(p1.rendreX()-v.rendreX(), p1.rendreY()-v.rendreY());
+
+		Point p3 = new Point();
+		p3.translation(p2.rendreX()-v.rendreX(),p2.rendreY()-v.rendreY());
+		tab_mem.set(0,p3);
+		tab_mem.set(1,p4);
+
+		System.out.println(tab_mem);
+
 	}
 
 
